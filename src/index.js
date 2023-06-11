7dd1262e5cbfac51088b307e850b8a125e326fe0
Simona <simonapalazova@gmail.com>
2023-06-11 20:32:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,8 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = require('./config/config')[env];
 const app = require('express')();
-//const homeController = require('./controllers/homeController');
-//const cubeController = require('./controllers/cubeController');
-
 
 require('./config/express')(app);
 require('./config/routes')(app);
-
-//app.use(homeController); 
-//app.use('/cubes', cubeController);
-//app.get('*', (req, res) => {
-// res.redirect('/404');
-//})
 
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
