2d948cea610589c5d3cb8b4b965a505abc624947
Simona <simonapalazova@gmail.com>
2023-06-11 14:54:10 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hi');
+const env = process.env.NODE_ENV || 'development';
+
+const config = require('./config/config')[env];
+const app = require('express')();
+
+require('./config/express')(app);
+require('./config/routes')(app);
+
+app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
