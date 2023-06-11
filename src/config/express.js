ba8422e53b1e8a440c6c6b4129bcaed0f4ec6c59
Simona <simonapalazova@gmail.com>
2023-06-11 15:16:30 +0300

    add handlebars

--- src/config/express.js
+++ src/config/express.js
@@ -5,7 +5,11 @@ const bodyParser = require('body-parser');
 module.exports = (app) => {
     
     //TODO: Setup the view engine
-
+    app.engine('hbs', handlebars.engine({
+        extname: 'hbs'
+    }));
+    app.set('view engine', 'hbs');
+    app.set('views', 'src/views')
     //TODO: Setup the body parser
 
     //TODO: Setup the static files
