a4e6b7ef35487a47cccdd5318942243d8e3830d7
Simona <simonapalazova@gmail.com>
2023-06-11 18:17:54 +0300

    save data

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,8 +1,27 @@
 const router = require('express').Router();
+const cubeManager = require('../managers/cubeManager');
 
 //Path is /cubes/create
 router.get('/create', (req, res) =>{
+    console.log(cubeManager.getAll());
     res.render('create');
+});
+
+router.post('/create', (req, res) =>{
+    const { 
+        name, 
+        description, 
+        imageUrl, 
+        difficultyLevel,
+    } = req.body;
+    
+    cubeManager.create({ 
+        name, 
+        description, 
+        imageUrl, 
+        difficultyLevel,
+    });
+    res.redirect('/');
 });
 
 module.exports = router;
